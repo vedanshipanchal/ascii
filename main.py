3a78a042b96662b7ccc26678033590dfdb6d0912
vedanshipanchal <vedanshipanchal@gmail.com>
2024-12-28 15:47:33 +0000

    ascii

---
+++ main.py
@@ -0,0 +1,13 @@
+#display ASCII code of characters
+def display_ascii_code():
+
+#input from the user
+
+input("please enter a character: ")
+
+if len == 1:
+    display_ascii_code = ord
+    print("please only enter one character: ")
+
+display_ascii_code
+
